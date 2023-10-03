d7605f7d257b6e7ead52a49dca3e8442f4dfe71d
koala819 <x.genolhac@gmail.com>
2023-10-03 15:44:30 +0200

    change SVG check by react icon & button

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { Chip } from "@nextui-org/react";
+import { Button, Chip } from "@nextui-org/react";
 import Link from "next/link";
+import { AiFillCheckCircle } from "react-icons/ai";
+
 export default function Page() {
   return (
     <>
@@ -33,105 +35,48 @@ export default function Page() {
 
               <div className='mt-8 space-y-8'>
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Responsive
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Rapidité
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Personnalisée
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Incitatif
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Stack JS
                   </span>
                 </div>
               </div>
 
-              <Link href='/contact'>
-                <button className='w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+              <Link
+                href='/contact'
+                className='flex items-center justify-center p-8'
+              >
+                <Button color='primary' variant='shadow'>
                   Demander un devis
-                </button>
+                </Button>
               </Link>
             </section>
             <section className='px-6 py-4 transition-colors duration-200 transform rounded-lg hover:bg-gray-300 dark:hover:bg-gray-900 bg-gray-200 dark:bg-gray-700'>
@@ -150,104 +95,47 @@ export default function Page() {
 
               <div className='mt-8 space-y-8'>
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Professionnalisme
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Visibilité en ligne
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Accessibilité
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Conception attrayante
                   </span>
                 </div>
 
                 <div className='flex items-center'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='w-5 h-5 text-blue-500'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-
+                  <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                   <span className='mx-4 text-gray-700 dark:text-gray-300'>
                     Processus itératifs
                   </span>
                 </div>
               </div>
-              <Link href='/contact'>
-                <button className='w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+              <Link
+                href='/contact'
+                className='flex items-center justify-center p-8'
+              >
+                <Button color='primary' variant='shadow'>
                   Demander un devis
-                </button>
+                </Button>
               </Link>
             </section>
             <div className='relative'>
@@ -270,98 +158,41 @@ export default function Page() {
 
                 <div className='mt-8 space-y-8'>
                   <div className='flex items-center'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='w-5 h-5 text-blue-500'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-
+                    <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                     <span className='mx-4 text-gray-300'>Intéractif</span>
                   </div>
 
                   <div className='flex items-center'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='w-5 h-5 text-blue-500'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-
+                    <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                     <span className='mx-4 text-gray-300'>Performant</span>
                   </div>
 
                   <div className='flex items-center'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='w-5 h-5 text-blue-500'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-
+                    <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                     <span className='mx-4 text-gray-300'>Database</span>
                   </div>
 
                   <div className='flex items-center'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='w-5 h-5 text-blue-500'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-
+                    <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                     <span className='mx-4 text-gray-300'>
                       Qualité exceptionnelle
                     </span>
                   </div>
 
                   <div className='flex items-center'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      className='w-5 h-5 text-blue-500'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z'
-                        clipRule='evenodd'
-                      />
-                    </svg>
-
+                    <AiFillCheckCircle className='w-5 h-5 text-blue-500' />
                     <span className='mx-4 text-gray-300'>
                       Partenariat Actif
                     </span>
                   </div>
                 </div>
-                <Link href='/contact'>
-                  <button className='w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600'>
+                <Link
+                  href='/contact'
+                  className='flex items-center justify-center p-8'
+                >
+                  <Button color='primary' variant='shadow'>
                     Demander un devis
-                  </button>
+                  </Button>
                 </Link>
               </section>
             </div>
