74a8a1691ea1ab1d914e7a50521c01bb194a4df1
koala819 <x.genolhac@gmail.com>
2023-10-07 15:59:07 +0200

    transform js to ts

--- src/app/GoogleAnalytics.tsx
+++ src/app/GoogleAnalytics.tsx
@@ -4,9 +4,6 @@ import Script from "next/script";
 import * as gtag from "@/lib/gtag";
 
 const GoogleAnalytics = () => {
-  //You can show in the console the GA_TRACKING_ID to confirm
-  console.log(gtag.GA_TRACKING_ID);
-
   return (
     <>
       <Script
