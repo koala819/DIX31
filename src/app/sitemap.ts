dec9d41f8eb86c1b76b62e7bf939f2dcac448250
koala819 <x.genolhac@gmail.com>
2023-11-14 11:07:40 +0100

    change with good url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,40 +3,16 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.activ-savoirs.com/",
+      url: "https://www.dix31.com/",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://www.activ-savoirs.com/contact",
+      url: "https://www.dix31.com/blog",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "daily",
       priority: 1,
-    },
-    {
-      url: "https://www.activ-savoirs.com/thematiques",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.activ-savoirs.com/references",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.activ-savoirs.com/equipe",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://www.activ-savoirs.com/evaluations",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
     },
   ];
 }
