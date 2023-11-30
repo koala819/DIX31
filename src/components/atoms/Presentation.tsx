0e0f2f8b4a29863a9d0a7760f1e8854f4c30d791
koala819 <x.genolhac@gmail.com>
2023-11-30 11:27:51 +0100

    improve Presentation code

--- src/components/atoms/Presentation.tsx
+++ src/components/atoms/Presentation.tsx
@@ -7,88 +7,85 @@ import Link from "next/link";
 
 export default function Presentation() {
   return (
-    <section>
-      <div className='mx-auto max-w-2xl px-4 py-16 sm:py-24 lg:max-w-7xl lg:px-8'>
-        <div className='flex justify-between items-center'>
-          <h2 className='text-2xl font-bold tracking-tight text-gray-900'>
-            Une solution web sur mesure pour les entrepreneurs et les
-            associations
-          </h2>
-        </div>
-        <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-          <Link
-            href='https://glittery-lebkuchen-a1a108.netlify.app/contact'
-            target='_blank'
-            className='group relative cursor-pointer'
-          >
-            <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
-              <Image
-                src={StarterPack_formulaire_contact}
-                alt='Screenhot du Formulaire de contact'
-                className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                width={300}
-                height={300}
-              />
-            </picture>
-            <h3 className='mt-4 text-sm text-gray-700'>
-              Formulaire de contact intégré et fonctionnel
-            </h3>
-          </Link>
-          <Link
-            href='https://glittery-lebkuchen-a1a108.netlify.app'
-            target='_blank'
-            className='group relative cursor-pointer'
-          >
-            <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
-              <Image
-                src={StarterPack_mode_sombre}
-                alt='Product image'
-                className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                width={300}
-                height={300}
-              />
-            </picture>
-            <h3 className='mt-4 text-sm text-gray-700'>
-              Basculer facilement entre les thèmes clair et sombre
-            </h3>
-          </Link>
-          <Link
-            href='https://github.com/koala819/starterPack/blob/master/site/src/components/util/GoogleAnalytics.tsx'
-            target='_blank'
-            className='group relative cursor-pointer'
-          >
-            <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
-              <Image
-                src={StarterPack_google_analytics}
-                alt='Code Google Analytics intégré'
-                className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                width={300}
-                height={300}
-              />
-            </picture>
-            <h3 className='mt-4 text-sm text-gray-700'>
-              Intégration de Google Analytics pour des insights précis
-            </h3>
-          </Link>
-          <Link
-            href='https://github.com/koala819/starterPackch'
-            target='_blank'
-            className='group relative cursor-pointer'
-          >
-            <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
-              <Image
-                src={StartPack_gratuit_libre}
-                alt='Screenshot repo GitHub'
-                className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                width={300}
-                height={300}
-              />
-            </picture>
-            <h3 className='mt-4 text-sm text-gray-700'>
-              Entièrement gratuit et open-source
-            </h3>
-          </Link>
-        </div>
+    <section className='mx-auto max-w-2xl px-4 py-16 sm:py-24 lg:max-w-7xl lg:px-8'>
+      <div className='flex justify-between items-center'>
+        <h2 className='text-2xl font-bold tracking-tight text-gray-900'>
+          Une solution web sur mesure pour les entrepreneurs et les associations
+        </h2>
+      </div>
+      <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+        <Link
+          href='https://glittery-lebkuchen-a1a108.netlify.app/contact'
+          target='_blank'
+          className='group relative cursor-pointer'
+        >
+          <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
+            <Image
+              src={StarterPack_formulaire_contact}
+              alt='Screenhot du Formulaire de contact'
+              className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+              width={300}
+              height={300}
+            />
+          </picture>
+          <h3 className='mt-4 text-sm text-gray-700'>
+            Formulaire de contact intégré et fonctionnel
+          </h3>
+        </Link>
+        <Link
+          href='https://glittery-lebkuchen-a1a108.netlify.app'
+          target='_blank'
+          className='group relative cursor-pointer'
+        >
+          <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
+            <Image
+              src={StarterPack_mode_sombre}
+              alt='Product image'
+              className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+              width={300}
+              height={300}
+            />
+          </picture>
+          <h3 className='mt-4 text-sm text-gray-700'>
+            Basculer facilement entre les thèmes clair et sombre
+          </h3>
+        </Link>
+        <Link
+          href='https://github.com/koala819/starterPack/blob/master/site/src/components/util/GoogleAnalytics.tsx'
+          target='_blank'
+          className='group relative cursor-pointer'
+        >
+          <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
+            <Image
+              src={StarterPack_google_analytics}
+              alt='Code Google Analytics intégré'
+              className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+              width={300}
+              height={300}
+            />
+          </picture>
+          <h3 className='mt-4 text-sm text-gray-700'>
+            Intégration de Google Analytics pour des insights précis
+          </h3>
+        </Link>
+        <Link
+          href='https://github.com/koala819/starterPackch'
+          target='_blank'
+          className='group relative cursor-pointer'
+        >
+          <picture className='aspect-square w-full overflow-hidden rounded-md opacity-75 hover:opacity-100 lg:h-80'>
+            <Image
+              src={StartPack_gratuit_libre}
+              alt='Screenshot repo GitHub'
+              className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+              width={300}
+              height={300}
+            />
+          </picture>
+          <h3 className='mt-4 text-sm text-gray-700'>
+            Entièrement gratuit et open-source
+          </h3>
+        </Link>
       </div>
     </section>
   );
