4c6ce83a14b3c1695450d0cb085a66cf2e72a72d
koala819 <x.genolhac@gmail.com>
2023-12-27 15:07:57 +0100

    BlogHeader creation

---
+++ src/components/atoms/BlogHeader.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Post } from "@/types/models";
+import { Date as DateComp } from "@/components/atoms/Date";
+
+export function BlogHeader({ firstPosts }: { firstPosts: Post[] }) {
+  return (
+    <header className='block md:flex md:space-x-2 px-2 lg:p-0'>
+      {firstPosts.map((post) => (
+        <Link
+          key={post.slug}
+          className='mb-4 md:mb-0 w-full md:w-1/2 relative rounded-lg inline-block'
+          href={`/blog/${post.slug}`}
+          style={{ height: "250px" }}
+        >
+          <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10 rounded-lg'></div>
+          <div className='relative w-full h-full rounded-lg overflow-hidden'>
+            <Image
+              src={post.picture || ""}
+              alt={post.title || ""}
+              layout='fill'
+              objectFit='cover'
+            />
+            <span className='px-4 py-1 bg-black text-white inline-flex items-center justify-center mb-2 rounded absolute top-4 left-4 z-10'>
+              {post.tags[0]}
+            </span>
+          </div>
+          <div className='p-4 absolute bottom-0 left-0 z-20'>
+            <h2 className='text-4xl font-semibold text-gray-100 leading-tight'>
+              {post.title}
+            </h2>
+            <div className='flex mt-3'>
+              <div>
+                <p className='font-semibold text-gray-400 text-xs'>
+                  <DateComp dateString={post.date} />
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </header>
+  );
+}
--- src/components/templates/Blog.tsx
+++ src/components/templates/Blog.tsx
@@ -2,11 +2,11 @@
 import React, { useState, useEffect } from "react";
 import PostPreview from "@/components/molecules/PostPreview";
 import { Chip } from "@nextui-org/react";
-
 import Image from "next/image";
 import { allPosts } from ".contentlayer/generated";
 import { Post } from "@/types/models";
 import Link from "next/link";
+import { BlogHeader } from "@/components/atoms/BlogHeader";
 
 export default function Blog() {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
@@ -38,163 +38,196 @@ export default function Blog() {
     setSelectedTag(null);
   };
 
+  function getTwoMostRecentPosts() {
+    return [...allPosts]
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+      .slice(0, 2);
+  }
+
+  const mostRecentPosts = getTwoMostRecentPosts();
+
   return (
     <div className='max-w-screen-xl mx-auto'>
-      <header className='block md:flex md:space-x-2 px-2 lg:p-0'>
-        <Link
-          className='mb-4 md:mb-0 w-full md:w-1/2 relative rounded-lg inline-block'
-          href='#'
-          style={{ height: "250px" }}
-        >
-          <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10 rounded-lg'></div>
-          <div className='relative w-full h-full rounded-lg overflow-hidden'>
-            <Image
-              src='https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
-              alt="Description de l'image"
-              layout='fill'
-              objectFit='cover'
-            />
-            <span className='px-4 py-1 bg-black text-white inline-flex items-center justify-center mb-2 rounded absolute bottom-4 right-4 z-10'>
-              Nutrition
-            </span>
-          </div>
-          <div className='p-4 absolute bottom-0 left-0 z-20'>
-            <h2 className='text-4xl font-semibold text-gray-100 leading-tight'>
-              Pellentesque a consectetur velit, ac molestie ipsum. Donec
-              sodales, massa et auctor.
-            </h2>
-            <div className='flex mt-3'>
-              {/* ... */}
-              <div>
-                <p className='font-semibold text-gray-200 text-sm'>
-                  Mike Sullivan
-                </p>
-                <p className='font-semibold text-gray-400 text-xs'> 14 Aug </p>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <Link
-          className='mb-4 md:mb-0 w-full md:w-1/2 relative rounded-lg inline-block'
-          href='#'
-          style={{ height: "250px" }}
-        >
-          <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10 rounded-lg'></div>
-          <div className='relative w-full h-full rounded-lg overflow-hidden'>
-            <Image
-              src='https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80'
-              alt="Description de l'image"
-              layout='fill'
-              objectFit='cover'
-            />
-            <span className='px-4 py-1 bg-black text-white inline-flex items-center justify-center mb-2 rounded absolute bottom-4 right-4 z-10'>
-              Science
-            </span>
-          </div>
-          <div className='p-4 absolute bottom-0 left-0 z-20'>
-            <h2 className='text-4xl font-semibold text-gray-100 leading-tight'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </h2>
-            <div className='flex mt-3'>
-              {/* ... */}
-              <div>
-                <p className='font-semibold text-gray-200 text-sm'>
-                  Mike Sullivan
-                </p>
-                <p className='font-semibold text-gray-400 text-xs'> 14 Aug </p>
-              </div>
-            </div>
-          </div>
-        </Link>
-      </header>
+      <BlogHeader firstPosts={mostRecentPosts} />
 
       <section className='block lg:flex lg:space-x-2 px-2 lg:p-0 mt-10 mb-10'>
         {/* Post Card */}
-        {/* <div className="w-full lg:w-2/3">
+        <aside className='w-full lg:w-2/3'>
+          <Link className='block rounded w-full lg:flex mb-10' href='#'>
+            <div
+              className='h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75 rounded-2xl'
+              style={{
+                backgroundImage:
+                  "url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')",
+              }}
+              title='deit is very important'
+            ></div>
 
-          <a className="block rounded w-full lg:flex mb-10"
-            href="#"
-          >
-            <div 
-              className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" 
-              title="deit is very important"
-            >
-            </div>
-            <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
+            <div className='bg-white rounded px-4 flex flex-col justify-between leading-normal'>
               <div>
-                <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                  Aliquam venenatis nisl id purus rhoncus, in efficitur sem hendrerit.
+                <div className='mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2'>
+                  Aliquam venenatis nisl id purus rhoncus, in efficitur sem
+                  hendrerit.
                 </div>
-                <p className="text-gray-700 text-base">
-                  Duis euismod est quis lacus elementum, eu laoreet dolor consectetur. 
-                  Pellentesque sed neque vel tellus lacinia elementum. Proin consequat ullamcorper eleifend.
+                <p className='text-gray-700 text-base'>
+                  Duis euismod est quis lacus elementum, eu laoreet dolor
+                  consectetur. Pellentesque sed neque vel tellus lacinia
+                  elementum. Proin consequat ullamcorper eleifend.
                 </p>
               </div>
-              <div className="flex mt-3">
-                <img src="https://randomuser.me/api/portraits/men/86.jpg"
-                  className="h-10 w-10 rounded-full mr-2 object-cover" />
+              <div className='flex mt-3'>
+                <Image
+                  alt='#'
+                  src='https://randomuser.me/api/portraits/men/86.jpg'
+                  className='h-10 w-10 rounded-full mr-2 object-cover'
+                  width={40}
+                  height={40}
+                />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className='font-semibold text-gray-700 text-sm capitalize'>
+                    {" "}
+                    eduard franz{" "}
+                  </p>
+                  <p className='text-gray-600 text-xs'> 14 Aug </p>
                 </div>
               </div>
             </div>
-          </a>
+          </Link>
 
-          <div className="rounded w-full lg:flex mb-10">
-            <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" title="deit is very important">
-            </div>
-            <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
+          <div className='rounded w-full lg:flex mb-10'>
+            <div
+              className='h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75 rounded-2xl'
+              style={{
+                backgroundImage:
+                  "url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')",
+              }}
+              title='deit is very important'
+            ></div>
+            <div className='bg-white rounded px-4 flex flex-col justify-between leading-normal'>
               <div>
-                <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
+                <div className='mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2'>
                   Integer commodo, sapien ut vulputate viverra
                 </div>
-                <p className="text-gray-700 text-base">
-                  Nam malesuada aliquet metus, ac commodo augue mollis sit amet. 
-                  Nam bibendum risus sit amet metus semper consectetur.
-                  Proin consequat ullamcorper eleifend.
-                  Nam bibendum risus sit amet metus semper consectetur.
+                <p className='text-gray-700 text-base'>
+                  Nam malesuada aliquet metus, ac commodo augue mollis sit amet.
+                  Nam bibendum risus sit amet metus semper consectetur. Proin
+                  consequat ullamcorper eleifend. Nam bibendum risus sit amet
+                  metus semper consectetur.
                 </p>
               </div>
-              <div className="flex mt-3">
-                <img src="https://randomuser.me/api/portraits/women/54.jpg"
-                  className="h-10 w-10 rounded-full mr-2 object-cover" />
+              <div className='flex mt-3'>
+                <Image
+                  alt='#'
+                  src='https://randomuser.me/api/portraits/women/54.jpg'
+                  className='h-10 w-10 rounded-full mr-2 object-cover'
+                  width={40}
+                  height={40}
+                />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> Serenity Hughes </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className='font-semibold text-gray-700 text-sm capitalize'>
+                    {" "}
+                    Serenity Hughes{" "}
+                  </p>
+                  <p className='text-gray-600 text-xs'> 14 Aug </p>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="rounded w-full lg:flex mb-10">
-            <div className="h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75"
-              style="background-image: url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')" title="deit is very important">
-            </div>
-            <div className="bg-white rounded px-4 flex flex-col justify-between leading-normal">
+          <div className='rounded w-full lg:flex mb-10'>
+            <div
+              className='h-48 lg:w-48 flex-none bg-cover text-center overflow-hidden opacity-75 rounded-2xl'
+              style={{
+                backgroundImage:
+                  "url('https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80')",
+              }}
+              title='deit is very important'
+            ></div>
+            <div className='bg-white rounded px-4 flex flex-col justify-between leading-normal'>
               <div>
-                <div className="mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2">
-                  Suspendisse varius justo eu risus laoreet fermentum non aliquam dolor
+                <div className='mt-3 md:mt-0 text-gray-700 font-bold text-2xl mb-2'>
+                  Suspendisse varius justo eu risus laoreet fermentum non
+                  aliquam dolor
                 </div>
-                <p className="text-gray-700 text-base">
-                  Mauris porttitor, velit at tempus vulputate, odio turpis facilisis dui, 
-                  vitae eleifend odio ipsum at odio. Phasellus luctus scelerisque felis eget suscipit. 
+                <p className='text-gray-700 text-base'>
+                  Mauris porttitor, velit at tempus vulputate, odio turpis
+                  facilisis dui, vitae eleifend odio ipsum at odio. Phasellus
+                  luctus scelerisque felis eget suscipit.
                 </p>
               </div>
-              <div className="flex mt-3">
-                <img src="https://randomuser.me/api/portraits/men/86.jpg"
-                  className="h-10 w-10 rounded-full mr-2 object-cover" />
+              <div className='flex mt-3'>
+                <Image
+                  alt='#'
+                  src='https://randomuser.me/api/portraits/men/86.jpg'
+                  className='h-10 w-10 rounded-full mr-2 object-cover'
+                  width={40}
+                  height={40}
+                />
                 <div>
-                  <p className="font-semibold text-gray-700 text-sm capitalize"> eduard franz </p>
-                  <p className="text-gray-600 text-xs"> 14 Aug </p>
+                  <p className='font-semibold text-gray-700 text-sm capitalize'>
+                    {" "}
+                    eduard franz{" "}
+                  </p>
+                  <p className='text-gray-600 text-xs'> 14 Aug </p>
                 </div>
               </div>
             </div>
           </div>
-
-        </div> */}
+        </aside>
+        {/* Right Sidebar */}
+        <aside className='w-full lg:w-1/3 px-3'>
+          <div className='mb-4'>
+            <h5 className='font-bold text-lg uppercase text-gray-700 px-1 mb-2'>
+              Mots clés
+            </h5>
+            <ul>
+              <li className='px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300'>
+                <a
+                  href='#'
+                  className='flex items-center text-gray-600 cursor-pointer'
+                >
+                  <span className='inline-block h-4 w-4 bg-green-300 mr-3'></span>
+                  Nutrition
+                  <span className='text-gray-500 ml-auto'>23 articles</span>
+                  <i className='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
+                </a>
+              </li>
+              <li className='px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300'>
+                <a
+                  href='#'
+                  className='flex items-center text-gray-600 cursor-pointer'
+                >
+                  <span className='inline-block h-4 w-4 bg-indigo-300 mr-3'></span>
+                  Food & Diet
+                  <span className='text-gray-500 ml-auto'>18 articles</span>
+                  <i className='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
+                </a>
+              </li>
+              <li className='px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300'>
+                <a
+                  href='#'
+                  className='flex items-center text-gray-600 cursor-pointer'
+                >
+                  <span className='inline-block h-4 w-4 bg-yellow-300 mr-3'></span>
+                  Workouts
+                  <span className='text-gray-500 ml-auto'>34 articles</span>
+                  <i className='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
+                </a>
+              </li>
+              <li className='px-1 py-4 border-b border-t border-white hover:border-gray-200 transition duration-300'>
+                <a
+                  href='#'
+                  className='flex items-center text-gray-600 cursor-pointer'
+                >
+                  <span className='inline-block h-4 w-4 bg-blue-300 mr-3'></span>
+                  Immunity
+                  <span className='text-gray-500 ml-auto'>9 articles</span>
+                  <i className='text-gray-500 bx bx-right-arrow-alt ml-1'></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
       </section>
 
       {/* ORIGINAL CODE */}
