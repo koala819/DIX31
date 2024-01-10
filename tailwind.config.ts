a7cf5a3e056b9c9230bae3abf6a0fbd5ff7e73f1
koala819 <x.genolhac@gmail.com>
2024-01-10 22:24:03 +0100

    fix issue bad display

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react')
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './ui/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class",
-  theme: {    
+  darkMode: 'class',
+  theme: {
     extend: {
       colors: {},
     },
   },
   plugins: [nextui(), require('@tailwindcss/typography')],
-};
+}
 
-module.exports = config;
+module.exports = config
