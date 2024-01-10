92da04f4f3617cb593d9d0e5f3b17aea13e37e8c
koala819 <x.genolhac@gmail.com>
2024-01-10 06:57:06 +0100

    clean Footer code with remove h1

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -68,9 +68,9 @@ export const Footer = () => {
       <div className="container px-6 py-12 mx-auto">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
           <div className="sm:col-span-2">
-            <h1 className="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl dark:text-white">
+            <span className="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl dark:text-white">
               Abonnez-vous à ma newsletter pour rester informé.
-            </h1>
+            </span>
             <form
               onSubmit={handleSubmit(handleSendMail)}
               className="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row"
@@ -162,9 +162,6 @@ export const Footer = () => {
         <hr className="my-6 border-gray-200 md:my-8 dark:border-gray-700" />
 
         <div className="flex items-center justify-between">
-          {/* <a href="#">
-                <img className="w-auto h-7" src="https://merakiui.com/images/full-logo.svg" alt="">
-            </a> */}
           <div className="w-full mt-2 text-sm md:text-xs lg:text-lg text-gray-500 dark:text-slate-400 ">
             <Image
               src={dix31Logo}
