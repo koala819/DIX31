de2a37c1b0f72ddaff883bff040301bfd1aacfda
koala819 <x.genolhac@gmail.com>
2024-01-10 20:32:31 +0100

    fix contentlayer.config

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,52 +1,52 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
 
 /**@type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
   slug: {
-    type: "string",
+    type: 'string',
     resolve: (post) => `${post._raw.flattenedPath}`,
   },
-};
+}
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
+  name: 'Post',
   filePathPattern: `**/*.mdx`,
-  contentType: "mdx",
+  contentType: 'mdx',
   fields: {
     title: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     description: {
-      type: "string",
+      type: 'string',
     },
     published: {
-      type: "boolean",
+      type: 'boolean',
       default: true,
     },
     date: {
-      type: "date",
+      type: 'date',
       required: true,
     },
     picture: {
-      type: "string",
+      type: 'string',
       require: true,
     },
     slug: {
-      type: "string",
+      type: 'string',
       require: true,
     },
-    tags: { type: "list", of: { type: "string" }, default: [] },
+    tags: { type: 'list', of: { type: 'string' }, default: [] },
   },
   computedFields,
-}));
+}))
 
 export default makeSource({
-  contentDirPath: "src/posts",
+  contentDirPath: 'posts',
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
@@ -55,20 +55,20 @@ export default makeSource({
       [
         rehypePrettyCode,
         {
-          theme: "github-light",
-          darkTheme: "github-dark",
+          theme: 'github-light',
+          darkTheme: 'github-dark',
           onVisitLine(node) {
             // Prevent lines from collapsing in 'display: grid' mode, and allow empty
             // lines to be copy/pasted
             if (node.children.length === 0) {
-              node.children = [{ type: "text", value: "" }];
+              node.children = [{ type: 'text', value: '' }]
             }
           },
           onVisitHighlightedLine(node) {
-            node.properties.className.push("line --highlighted");
+            node.properties.className.push('line --highlighted')
           },
           onVisitHighlightedWord(node) {
-            node.properties.className.push("word --highlighted");
+            node.properties.className.push('word --highlighted')
           },
         },
       ],
@@ -76,11 +76,11 @@ export default makeSource({
         rehypeAutolinkHeadings,
         {
           properties: {
-            className: ["subheading-anchor"],
-            arialabel: "Link to section",
+            className: ['subheading-anchor'],
+            arialabel: 'Link to section',
           },
         },
       ],
     ],
   },
-});
+})
