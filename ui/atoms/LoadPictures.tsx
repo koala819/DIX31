28174e8edb124540e37ffc982c842e6ccf901d91
koala819 <x.genolhac@gmail.com>
2024-05-13 09:17:28 +0200

    LazyLoad for pictures

---
+++ ui/atoms/LoadPictures.tsx
@@ -0,0 +1,58 @@
+import { Image as ImageReact } from '@nextui-org/react'
+
+import Image from 'next/image'
+
+const LoadPictures = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+  objectFit,
+  layout,
+}: {
+  src: any
+  alt: string
+  width?: number
+  height?: number
+  className?: string
+  objectFit?: string
+  layout?: string
+}) => {
+  return (
+    <div>
+      {layout === 'fill' ? (
+        <Image
+          src={src}
+          alt={alt}
+          loading="lazy"
+          className={className}
+          objectFit={objectFit}
+          layout={layout}
+        />
+      ) : layout === 'responsive' ? (
+        <Image
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          loading="lazy"
+          className={className}
+          objectFit={objectFit}
+          layout={layout}
+        />
+      ) : (
+        <ImageReact
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          loading="lazy"
+          className={className}
+        />
+      )}
+    </div>
+  )
+}
+
+export default LoadPictures
