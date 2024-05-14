28d32de23c1c3e3cfdb11b928ad42a58d01c330e
koala819 <x.genolhac@gmail.com>
2024-05-14 08:47:52 +0200

    remove all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import { Analytics } from '@vercel/analytics/react'
-import { Suspense, lazy } from 'react'
-
+// import { Analytics } from '@vercel/analytics/react'
+// import { Suspense, lazy } from 'react'
 import { Metadata } from 'next'
 
 // import { Inter } from 'next/font/google'
-import { ThemeProviders } from '@/app/provider'
-import '@/styles/globals.css'
-import GoogleAnalytics from '@/ui/atoms/GoogleAnalytics'
+// import { ThemeProviders } from '@/app/provider'
+
+// import '@/styles/globals.css'
+
+// import GoogleAnalytics from '@/ui/atoms/GoogleAnalytics'
 
 // import { Footer } from '@/ui/templates/Footer'
 // import { NavbarDIX as Navbar } from '@/ui/templates/Navbar'
 
-const Footer = lazy(() => import('@/ui/templates/Footer'))
-const Navbar = lazy(() => import('@/ui/templates/Navbar'))
+// const Footer = lazy(() => import('@/ui/templates/Footer'))
+// const Navbar = lazy(() => import('@/ui/templates/Navbar'))
 
 // const inter = Inter({
 //   weight: ['400', '700'],
@@ -44,26 +45,26 @@ export default function RootLayout({
       <body
       // className={`${inter.variable}`}
       >
-        <ThemeProviders>
-          <div className="min-h-screen min-w-screen">
-            <Suspense fallback={<div>Chargement ...</div>}>
+        {/* <ThemeProviders> */}
+        <div className="min-h-screen min-w-screen">
+          {/* <Suspense fallback={<div>Chargement ...</div>}>
               <Navbar />
-            </Suspense>
+            </Suspense> */}
 
-            <main className="flex-1">
-              <div className="w-full px-4 mx-auto mt-6">
-                <div className="flex flex-col min-w-0 break-words w-full mb-6 rounded-lg bg-gray-50 dark:bg-slate-800 border-0">
-                  <GoogleAnalytics />
-                  {children}
-                  <Analytics />
-                </div>
+          <main className="flex-1">
+            <div className="w-full px-4 mx-auto mt-6">
+              <div className="flex flex-col min-w-0 break-words w-full mb-6 rounded-lg bg-gray-50 dark:bg-slate-800 border-0">
+                {/* <GoogleAnalytics /> */}
+                {children}
+                {/* <Analytics /> */}
               </div>
-            </main>
-            <Suspense fallback={<div>Chargement ...</div>}>
+            </div>
+          </main>
+          {/* <Suspense fallback={<div>Chargement ...</div>}>
               <Footer />
-            </Suspense>
-          </div>
-        </ThemeProviders>
+            </Suspense> */}
+        </div>
+        {/* </ThemeProviders> */}
       </body>
     </html>
   )
