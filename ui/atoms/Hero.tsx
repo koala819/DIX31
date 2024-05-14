3992a8ee0225fd8dc39a217fcb2c06f0f191e52a
koala819 <x.genolhac@gmail.com>
2024-05-14 14:04:43 +0200

    add H1

--- ui/atoms/Hero.tsx
+++ ui/atoms/Hero.tsx
@@ -1,7 +1,11 @@
 export default function Hero() {
   return (
     <section className="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
-      PLOUF
+      <div className="flex flex-wrap justify-between">
+        <h1 className="text-4xl md:text-6xl font-bold text-gray-600 dark:text-gray-200 mb-6">
+          Créez avec Liberté, Vivez sans Limite
+        </h1>
+      </div>
     </section>
   )
 }
