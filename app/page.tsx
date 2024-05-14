66ff092db7f0d50757b076b8439aed9ff88c2061
koala819 <x.genolhac@gmail.com>
2024-05-14 07:30:55 +0200

    display only Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import React, { Suspense, lazy } from 'react'
-import { cache } from 'react'
-
+// import React, { Suspense, lazy } from 'react'
+// import { cache } from 'react'
 import { Metadata } from 'next'
 
-import { comments } from '@/utils/comments'
-
-import { client } from '@/lib/sanity'
+// import { comments } from '@/utils/comments'
+// import { client } from '@/lib/sanity'
 import Hero from '@/ui/atoms/Hero'
 
-const Blog = lazy(() => import('@/ui/atoms/Blog'))
-const Projets = lazy(() => import('@/ui/atoms/Projets'))
-const Rates = lazy(() => import('@/ui/atoms/Rates'))
-const CommentList = lazy(() => import('@/ui/molecules/CommentList'))
+// const Blog = lazy(() => import('@/ui/atoms/Blog'))
+// const Projets = lazy(() => import('@/ui/atoms/Projets'))
+// const Rates = lazy(() => import('@/ui/atoms/Rates'))
+// const CommentList = lazy(() => import('@/ui/molecules/CommentList'))
 
 export const metadata: Metadata = {
   title: 'DIX31 - Création de Sites Web Personnalisés sur mesure à Toulouse',
@@ -19,34 +17,34 @@ export const metadata: Metadata = {
     'Spécialiste en création de sites web personnalisés et responsive. Solutions sur mesure pour entreprises. Consultation gratuite.',
 }
 export default async function Page() {
-  const getPosts = cache(async () => {
-    'use server'
-    const query = `*[_type == 'blog'] | order(date desc)[0...3] {
-      title,
-      date,
-      smallDescription,
-      "currentSlug": slug.current,
-      titleImage,
-      titleImagebyCloudinary,
-      "tag": tag[]->{
-        name
-      }
-    }`
+  // const getPosts = cache(async () => {
+  //   'use server'
+  //   const query = `*[_type == 'blog'] | order(date desc)[0...3] {
+  //     title,
+  //     date,
+  //     smallDescription,
+  //     "currentSlug": slug.current,
+  //     titleImage,
+  //     titleImagebyCloudinary,
+  //     "tag": tag[]->{
+  //       name
+  //     }
+  //   }`
 
-    return await client.fetch(query)
-  })
+  //   return await client.fetch(query)
+  // })
 
-  const posts = await getPosts()
+  // const posts = await getPosts()
 
   return (
     <section className="my-16">
       <Hero />
-      <Suspense fallback={<div>Chargement ...</div>}>
+      {/* <Suspense fallback={<div>Chargement ...</div>}>
         <Blog posts={posts} />
         <Projets />
         <Rates />
         <CommentList comments={comments} />
-      </Suspense>
+      </Suspense> */}
     </section>
   )
 }
