c66f3eb6864912597ec5dff98d53e2d2b249ae95
koala819 <x.genolhac@gmail.com>
2024-05-15 09:42:04 +0200

    test

--- ui/atoms/Hero.tsx
+++ ui/atoms/Hero.tsx
@@ -1,33 +1,52 @@
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
-import LoadImage from '@/lib/LoadImage'
-
-// const WithCustomLoading = dynamic(() => import('@/lib/LoadImage'), {
-//   loading: () => <div>Chargement ...</div>,
-// })
+const WithCustomLoading = dynamic(() => import('@/ui/atoms/LoadPictures'), {
+  loading: () => <div>Chargement ...</div>,
+})
 
 export default function Hero() {
+  const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+  const toBase64 = (str: string) =>
+    typeof window === 'undefined'
+      ? Buffer.from(str).toString('base64')
+      : window.btoa(str)
+
   return (
     <section className="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8 ">
       <h1 className="font-sans text-4xl md:text-6xl font-bold text-gray-600 dark:text-gray-200 mb-6">
         Votre vision est ma mission !
       </h1>
       <section className="lg:flex">
-        {/* MOBILE VIEW DISPLAY */}
-        <aside className="w-full lg:hidden flex justify-center mb-4">
-          <LoadImage
+        <aside className="w-full lg:hidden">
+          <Image
             src="/images/Accompagnement-dix31-mobile.webp"
             alt="dix31 vous accompagne dans votre projet"
-            className="responsive rounded-2xl"
+            className="responsive"
+            placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
             width={350}
             height={200}
             priority
           />
         </aside>
-        <aside className="flex w-full flex-col justify-center lg:mb-0 lg:w-1/2 lg:pb-24">
+        <aside className="flex w-full flex-col justify-center lg:mb-0 lg:w-1/2 lg:pb-24 bg-blue-200">
           <div className="container mx-auto px-4">
             <p className="font-sans text-lg md:text-xl text-gray-700 dark:text-gray-300 mb-6">
               Libérons ensemble votre projet des complexités et des contraintes
@@ -44,12 +63,12 @@ export default function Hero() {
             </Link>
           </div>
         </aside>
-        <aside className="hidden lg:block lg:w-1/2">
+        <aside className="hidden lg:block lg:w-1/2 bg-purple-500">
           <picture className="hidden md:flex w-full justify-center p-8">
-            <LoadImage
+            <WithCustomLoading
               src="/images/Accompagnement-avec-dix31.webp"
               alt="dix31 vous accompagne dans votre projet"
-              className="responsive  rounded-3xl"
+              className="responsive"
               width={700}
               height={400}
               priority
