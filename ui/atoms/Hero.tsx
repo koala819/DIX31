5217628cfa0fc5bef08dc4005b8613df0d84c7c8
koala819 <x.genolhac@gmail.com>
2024-05-15 11:25:23 +0200

    test

--- ui/atoms/Hero.tsx
+++ ui/atoms/Hero.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
-import { Skeleton } from '@/components/ui/skeleton'
+
+// import { Skeleton } from '@/components/ui/skeleton'
+// import LoadPictures from '@/ui/atoms/LoadPictures'
 
 const WithCustomLoading = dynamic(() => import('@/ui/atoms/LoadPictures'), {
   loading: () => <div>Chargement ...</div>,
@@ -11,18 +13,18 @@ const WithCustomLoading = dynamic(() => import('@/ui/atoms/LoadPictures'), {
 
 export default function Hero() {
   const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`
 
   const toBase64 = (str: string) =>
     typeof window === 'undefined'
@@ -47,7 +49,8 @@ export default function Hero() {
             priority
           />
         </aside>
-        <aside className="flex w-full flex-col justify-center lg:mb-0 lg:w-1/2 lg:pb-24 bg-blue-200">
+
+        <aside className="flex w-full flex-col justify-center lg:mb-0 lg:w-1/2 lg:pb-24">
           <div className="container mx-auto px-4">
             <p className="font-sans text-lg md:text-xl text-gray-700 dark:text-gray-300 mb-6">
               Libérons ensemble votre projet des complexités et des contraintes
@@ -64,12 +67,14 @@ export default function Hero() {
             </Link>
           </div>
         </aside>
-        <aside className="hidden lg:block lg:w-1/2 bg-purple-500">
+
+        {/* DISPLAY LARGE VIEW */}
+        <aside className="hidden lg:block lg:w-1/2">
           <picture className="hidden md:flex w-full justify-center p-8">
             <WithCustomLoading
               src="/images/Accompagnement-avec-dix31.webp"
               alt="dix31 vous accompagne dans votre projet"
-              className="responsive"
+              className="responsive rounded-3xl"
               width={700}
               height={400}
               priority
