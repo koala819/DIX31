85e387b7e030994d2be083242fced50da525c87c
koala819 <x.genolhac@gmail.com>
2024-07-21 12:56:45 +0200

    fix or in NextjsContent

--- components/atoms/NextjsContent.tsx
+++ components/atoms/NextjsContent.tsx
@@ -60,7 +60,7 @@ const NextjsContent: React.FC = () => {
         >
           {t('recentWorkLink')}
         </Link>{' '}
-        ou{' '}
+        {t('or')}{' '}
         <Link
           className="text-blue-600 hover:text-blue-800 underline"
           href="/?scrollTo=contact"
