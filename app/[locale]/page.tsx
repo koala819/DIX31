d6340ccf934ff7bcff901dd5b194c9c9ac93de90
koala819 <x.genolhac@gmail.com>
2024-10-18 15:59:35 +0200

    fix: link with hireBtn

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,21 +1,8 @@
-import { Suspense } from 'react'
-
 import { Metadata } from 'next'
-import { getTranslations } from 'next-intl/server'
-import dynamic from 'next/dynamic'
 
-import Career from '@/components/atoms/Career'
-import FAQ from '@/components/atoms/Faq'
-import IdeaToLaunchSteps from '@/components/atoms/IdeaToLaunchSteps'
-import ScrollToContact from '@/components/atoms/ScrollToContact'
-import Services from '@/components/atoms/Services'
-import Contact from '@/components/molecules/Contact'
-import Testimonial from '@/components/molecules/Testimonial'
+import { Homepage } from '@/components/atoms/Homepage'
 
 import { createMetadata } from '@/lib/utils'
-
-const Hero = dynamic(() => import('@/components/atoms/Hero'))
-const Profile = dynamic(() => import('@/components/atoms/Profile'))
 
 export async function generateMetadata({
   params: { locale },
@@ -29,35 +16,6 @@ export async function generateMetadata({
   })
 }
 
-export default async function Page({
-  params: { locale },
-}: {
-  params: { locale: string }
-}) {
-  const t = await getTranslations({ locale })
-
-  return (
-    <main className="container space-y-12">
-      <ScrollToContact />
-      <Hero />
-      <Suspense
-        fallback={
-          <div role="alert" aria-busy="true">
-            {t('loading')}
-          </div>
-        }
-      >
-        <Profile />
-        <Career />
-        <Testimonial user="Joge" />
-        <Services />
-        <Testimonial user="Sandrine" />
-        <IdeaToLaunchSteps />
-        <Testimonial user="Pascal" />
-        <Contact id="contact" />
-        <Testimonial user="Sid" />
-        <FAQ />
-      </Suspense>
-    </main>
-  )
+export default async function FirstPage() {
+  return <Homepage />
 }
